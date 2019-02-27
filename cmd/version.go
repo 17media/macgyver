2284a580d4de5093779c73f4ab254b4cc70934c3
tobby17 <30612032+tobby17@users.noreply.github.com>
2019-02-27 10:30:33 +0800

    Update version & readme (#8)

--- cmd/version.go
+++ cmd/version.go
@@ -12,9 +12,9 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
+	Short: "Print the version number of Macgyver",
+	Long:  `All software has versions. This is Macgyver's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+		fmt.Println("v1.1.1")
 	},
 }
